90aab0a92a7e1b8914a54ce8d23ccc3b9b937a0b
aybarsiya <aybarsay@hotmail.com>
2024-09-26 23:36:15 -0400

    finished detecting the chessboard on a screenshot using a template image with a mask version of it

--- ScreenHandler.py
+++ ScreenHandler.py
@@ -5,59 +5,108 @@ from PIL import ImageGrab
 # 744
 
 class ScreenHandler:        
-        screenImg = cv.imread("screenshot.png", cv.IMREAD_COLOR)
+        _screenImg = cv.imread("screenshot.png", cv.IMREAD_COLOR)
         
-        tempImg = cv.imread("greenboard.png", cv.IMREAD_COLOR)
-
-        alphaChannel = np.array(cv.split(tempImg)[3])
-        maskImg = cv.merge([alphaChannel, alphaChannel, alphaChannel])
+        _tempImg = cv.imread("greenboard.png", cv.IMREAD_COLOR)
         
-        sizeOnScreen = tempImg.shape[:2]
+        print(_tempImg.shape)
 
-        minScale = float(40)
-        maxScale = float(90)
-        initialScaleStepPercentage = float(5)
+        _alphaChannel = np.array(cv.imread("greenboard_mask.png", cv.IMREAD_UNCHANGED)[:,:,3])
+        _maskImg = cv.merge([_alphaChannel, _alphaChannel, _alphaChannel])
+        
+        _sizeOnScreen = _tempImg.shape[:2]
+        
+        print(_sizeOnScreen)
+
+        _minScale = 0.4
+        _maxScale = 0.9
+        _initialScaleStepPercentage = 0.05
         
         def FindBestPosition(self):
-                closestValue = float
-                closestPosition = cv.typing.Point()
-                closestScale = float
                 
-                scaleStepPercentage = self.initialScaleStepPercentage
+                closestValue = float()
+                closestPosition = (0, 0)
+                closestScale = float()
                 
-                totalScaleSteps = self.updateTotalScaleSteps(self.initialScaleStepPercentage)
+                scaleStepPercentage = self._initialScaleStepPercentage
                 
-                while (totalScaleSteps <= 1):
-                        if(scaleStepPercentage == self.initialScaleStepPercentage):
-                                for scale in np.fliplr(np.linspace(self.minScale, self.maxScale, totalScaleSteps)):        
-                                        scaledTempImg = cv.resize(self.tempImg, self.sizeOnScreen * scale, interpolation = cv.INTER_LINEAR)
-                                        scaledMaskImg = cv.resize(self.maskImg, self.sizeOnScreen * scale, interpolation = cv.INTER_LINEAR)
+                totalScaleSteps = self._getTotalScaleSteps(self._initialScaleStepPercentage)
+                
+                print(totalScaleSteps)
+                
+                print(np.linspace(self._minScale, self._maxScale, totalScaleSteps), np.linspace(self._minScale, self._maxScale, totalScaleSteps)[::-1], sep = "\n\n")
+                
+                scaledSizeOnScreen = (0, 0)
+                
+                for scale in np.linspace(self._minScale, self._maxScale, totalScaleSteps)[::-1]:        
+                        
+                        scaledSizeOnScreen = (int(self._sizeOnScreen[0] * scale), int(self._sizeOnScreen[1] * scale))
+                        
+                        scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                        scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                
+                        _, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(self._screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                
+                        if(bestValue > closestValue):
                                 
-                                        _, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(self.screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                                closestValue = bestValue
+                                closestScale = float(scale)
                                 
-                                        if(bestValue > closest):
-                                                closestValue = bestValue
-                                        else:
-                                                scaleStepPercentage -= 1
-                                                break
+                                print(scaledSizeOnScreen, bestValue, closestScale, sep = '\n')
                         else:
-                                for scale in np.fliplr(np.linspace(self.minScale, self.maxScale, totalScaleSteps)):        
-                                        scaledTempImg = cv.resize(self.tempImg, self.sizeOnScreen * scale, interpolation = cv.INTER_LINEAR)
-                                        scaledMaskImg = cv.resize(self.maskImg, self.sizeOnScreen * scale, interpolation = cv.INTER_LINEAR)
+                                break
+                        
+                scaleStepPercentage = 0.01
+                
+                upScaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale + scaleStepPercentage)), int(self._sizeOnScreen[1] * (closestScale + scaleStepPercentage)))
+                downScaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale - scaleStepPercentage)), int(self._sizeOnScreen[1] * (closestScale + scaleStepPercentage)))
+                
+                scaledTempImg = cv.resize(self._tempImg, upScaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                scaledMaskImg = cv.resize(self._maskImg, upScaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                _, upBestValue, _, upBestLocation = cv.minMaxLoc(cv.matchTemplate(self._screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                
+                scaledTempImg = cv.resize(self._tempImg, downScaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                scaledMaskImg = cv.resize(self._maskImg, downScaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                _, downBestValue, _, downBestLocation = cv.minMaxLoc(cv.matchTemplate(self._screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                
+                if(downBestValue > upBestValue):
+                        
+                        scaleStepPercentage *= -1.0
+                        closestValue = downBestValue
+                        closestPosition = downBestLocation
+                else:
+                        
+                        closestValue = upBestValue
+                        closestPosition = upBestLocation
+
+                closestScale += scaleStepPercentage
+                        
+                while(True):
+                        scaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale + scaleStepPercentage)), int(self._sizeOnScreen[1] * scale))
+                        
+                        scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                        scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_LINEAR)
+                        
+                        _, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(self._screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                
+                        if(bestValue > closestValue):
+                                closestValue = bestValue
+                                closestPosition = bestLocation
+                                closestScale += scaleStepPercentage
                                 
-                                        _, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(self.screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
-                                
-                                        if(bestValue > closest):
-                                                closest = bestValue
-                                        else:
-                                                scaleStepPercentage -= 1
-                                                break
+                                print(scaledSizeOnScreen, bestValue, closestScale, sep = '\n')
+                        else:
+                                break
+                
+                print(closestValue, closestPosition, closestScale, sep = '\n')
+
+
                                 
         
         def _getTotalScaleSteps(self, scaleStepPercentage: float):
-                return (self.maxScale - self.minScale) / scaleStepPercentage
+                return int((self._maxScale - self._minScale) / scaleStepPercentage) + 1
                 
+sh = ScreenHandler()
 
-
-
+sh.FindBestPosition()
 
