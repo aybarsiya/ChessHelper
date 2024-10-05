41867ac050d4ce7f53f92d6b56af778b3b166c73
aybarsiya <aybarsay@hotmail.com>
2024-10-04 23:00:19 -0400

    downscaling the "screenshot" image gives unexpected results, trying to find a solution

--- ScreenHandler.py
+++ ScreenHandler.py
@@ -7,40 +7,56 @@ from PIL import ImageGrab as IG
 
 class ScreenHandler:        
         
-        # _screenImg = cv.imread("screenshot.png", cv.IMREAD_GRAYSCALE)        
-        _tempImg = cv.imread(r"resources\board\greenboard.png", cv.IMREAD_GRAYSCALE)
-        _maskImg = cv.merge([cv.imread(r"resources\board\greenboard_mask.png", cv.IMREAD_UNCHANGED)[:,:,3]])
+        _screenImgUntouched = cv.imread("screenshot2.png", cv.IMREAD_GRAYSCALE)        
+        _tempImgUntouched = cv.imread(r"resources\board\greenboard.png", cv.IMREAD_GRAYSCALE)
+        _maskImgUntouched = cv.merge([cv.imread(r"resources\board\greenboard_mask.png", cv.IMREAD_UNCHANGED)[:,:,3]])
+        
+        _screenImgDownsized = None
+        _tempImgDownsized = None
+        _maskImgDownsized = None
+        _downsizedScale = None
                 
-        _tempImgSize = _tempImg.shape[:2]
+        _tempImgSize = _tempImgUntouched.shape[:2]
         
         _downsizeScale = 0.500
+        
+        _downsizeInterpolation = cv.INTER_LINEAR
         
         async def FindChessboardPosition(self):
                 
                 # await aio.sleep(3)
                 screenshot = self._TakeScreenshot()
-                
-                cv.imshow("", screenshot)
-                
+                                
                 closestValue = float()
                 closestPosition = (int(), int())
                 closestScale = float()
                 
-                scaleStepPercentage = self._initialScaleStepPercentage
+                # scaleStepPercentage = self._initialScaleStepPercentage
                 
-                totalScaleSteps = int((self._maxScale - self._minScale) / scaleStepPercentage) + 1
+                # totalScaleSteps = int((self._maxScale - self._minScale) / scaleStepPercentage) + 1
                 
-                print(totalScaleSteps)
+                # print(totalScaleSteps)
                 
                 scaledSizeOnScreen = (0, 0)
                 
                 startTime = time.time()
                 
-                while(False):
+                scale = 0.900
+                
+                stime = time.time()
+                
+                self._DownsizeImgs()
+                
+                while(scale >= 0.400):
                         
+                        print(scale)
+                        print(self._FindMinMaxLoc(scale, self._screenImg))
+                        print("------")
+                        scale = round((scale - 0.100), 20)
                         
-                        
-                        pass
+                print(time.time() - stime)
+                
+                
                 
                 for scale in np.linspace(self._minScale, self._maxScale, totalScaleSteps)[::-1]:        
                         
@@ -117,17 +133,30 @@ class ScreenHandler:
         
         def _FindMinMaxLoc(self, scalePercentage: float, screenImg: cv.typing.MatLike) -> tuple[float, cv.typing.Point]:
                 
+                print(screenImg.shape)
                 scalePercentage = round(scalePercentage, 3)
                 scaledSizeOnScreen = tuple([int(x * scalePercentage) for x in self._tempImgSize])
-                                                
-                scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
-                scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
+                
+                screenImg = cv.resize(screenImg, tuple([int(x * self._downsizeScale) for x in screenImg.shape[:2]]), interpolation = self._downsizeInterpolation)
+                                                                                
+                scaledTempImg = cv.resize(self._tempImg.copy(), scaledSizeOnScreen, interpolation = self._downsizeInterpolation)
+                scaledMaskImg = cv.resize(self._maskImg.copy(), scaledSizeOnScreen, interpolation = self._downsizeInterpolation)
+                                
+                print(scaledTempImg.shape)
 
                 _1, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
                 
                 print(_1, bestValue, sep = ' ')
-                
+                                
                 return tuple[bestValue, bestLocation]
+        
+        def _DownsizeImgs(self):
+                
+                if(round(self._downsizedScale, 3) == round(self._downsizeScale, 3)) and (self._downsizedScale != None): return
+                
+                self._screenImgDownsized = cv.resize(self._screenImgUntouched, tuple([int(x * self._downsizeScale) for x in self._screenImgUntouched.shape[:2]]), interpolation = self._downsizeInterpolation)
+                self._tempImgDownsized = cv.resize(self._tempImgUntouched, tuple([int(x * self._downsizeScale) for x in self._tempImgUntouched.shape[:2]]), interpolation = self._downsizeInterpolation)
+                self._maskImgDownsized = cv.resize(self._maskImgUntouched, tuple([int(x * self._downsizeScale) for x in self._maskImgUntouched.shape[:2]]), interpolation = self._downsizeInterpolation)
         
         def _TakeScreenshot(self):
                 
