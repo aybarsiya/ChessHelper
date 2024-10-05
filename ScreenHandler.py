f379ef600714b458aaf23240798904c497d9ee68
aybarsiya <aybarsay@hotmail.com>
2024-10-04 20:31:27 -0400

    -added screenshot functionality -created a simplified general function to return "bestValue" and "bestLocation" values as a tuple -working on a solution to minimize "finding the chessboard on the screen" time (already at around 1.5s)

--- ScreenHandler.py
+++ ScreenHandler.py
@@ -1,21 +1,26 @@
 import cv2 as cv
 import numpy as np
 import time
-# from PIL import ImageGrab as imgGrab
+import asyncio as aio
+
+from PIL import ImageGrab as IG
 
 class ScreenHandler:        
         
-        _screenImg = cv.imread("screenshot2.png", cv.IMREAD_COLOR)        
-        _tempImg = cv.imread(r"resources\board\greenboard.png", cv.IMREAD_COLOR)
-        _maskImg = cv.merge([cv.imread(r"resources\board\greenboard_mask.png", cv.IMREAD_UNCHANGED)[:,:,3] * 3])
+        # _screenImg = cv.imread("screenshot.png", cv.IMREAD_GRAYSCALE)        
+        _tempImg = cv.imread(r"resources\board\greenboard.png", cv.IMREAD_GRAYSCALE)
+        _maskImg = cv.merge([cv.imread(r"resources\board\greenboard_mask.png", cv.IMREAD_UNCHANGED)[:,:,3]])
+                
+        _tempImgSize = _tempImg.shape[:2]
         
-        _sizeOnScreen = tuple(_tempImg.shape[:2])
-
-        _minScale = 0.4
-        _maxScale = 0.9
-        _initialScaleStepPercentage = 0.05
+        _downsizeScale = 0.500
         
-        def FindBestPosition(self):
+        async def FindChessboardPosition(self):
+                
+                # await aio.sleep(3)
+                screenshot = self._TakeScreenshot()
+                
+                cv.imshow("", screenshot)
                 
                 closestValue = float()
                 closestPosition = (int(), int())
@@ -31,15 +36,22 @@ class ScreenHandler:
                 
                 startTime = time.time()
                 
+                while(False):
+                        
+                        
+                        
+                        pass
+                
                 for scale in np.linspace(self._minScale, self._maxScale, totalScaleSteps)[::-1]:        
+                        
                         scaledSizeOnScreen = tuple[int]()
-                        scaledSizeOnScreen = (int(self._sizeOnScreen[0] * scale), int(self._sizeOnScreen[1] * scale))
+                        scaledSizeOnScreen = (int(self._tempImgSize[0] * scale), int(self._tempImgSize[1] * scale))
                         
                         scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
                         scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
-                
+
                         _, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(self._screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
-                
+                                        
                         if(bestValue > closestValue):
                                 
                                 closestValue = bestValue
@@ -51,8 +63,8 @@ class ScreenHandler:
                         
                 scaleStepPercentage = 0.002
                 
-                upScaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale + scaleStepPercentage)), int(self._sizeOnScreen[1] * (closestScale + scaleStepPercentage)))
-                downScaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale - scaleStepPercentage)), int(self._sizeOnScreen[1] * (closestScale - scaleStepPercentage)))
+                upScaledSizeOnScreen = (int(self._tempImgSize[0] * (closestScale + scaleStepPercentage)), int(self._tempImgSize[1] * (closestScale + scaleStepPercentage)))
+                downScaledSizeOnScreen = (int(self._tempImgSize[0] * (closestScale - scaleStepPercentage)), int(self._tempImgSize[1] * (closestScale - scaleStepPercentage)))
                 
                 scaledTempImg = cv.resize(self._tempImg, upScaledSizeOnScreen, interpolation = cv.INTER_AREA)
                 scaledMaskImg = cv.resize(self._maskImg, upScaledSizeOnScreen, interpolation = cv.INTER_AREA)
@@ -78,7 +90,7 @@ class ScreenHandler:
                 closestScaledSizeOnScreen = (0, 0)
                         
                 while(True):
-                        scaledSizeOnScreen = (int(self._sizeOnScreen[0] * (closestScale + scaleStepPercentage)), int(self._sizeOnScreen[1] * (closestScale + scaleStepPercentage)))
+                        scaledSizeOnScreen = (int(self._tempImgSize[0] * (closestScale + scaleStepPercentage)), int(self._tempImgSize[1] * (closestScale + scaleStepPercentage)))
                         
                         scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
                         scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
@@ -102,14 +114,34 @@ class ScreenHandler:
                 
                 print("===== RESULTS: ", "\nClosest Matching Value: ", closestValue, "\nClosest Matching Position: ", closestPosition, "\nClosest Matching Scale: ", closestScale, "\nClosest Matching Size: ", closestScaledSizeOnScreen, "\nTotal Time: ", totalTimeTook, sep = '')
 
+        
+        def _FindMinMaxLoc(self, scalePercentage: float, screenImg: cv.typing.MatLike) -> tuple[float, cv.typing.Point]:
+                
+                scalePercentage = round(scalePercentage, 3)
+                scaledSizeOnScreen = tuple([int(x * scalePercentage) for x in self._tempImgSize])
+                                                
+                scaledTempImg = cv.resize(self._tempImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
+                scaledMaskImg = cv.resize(self._maskImg, scaledSizeOnScreen, interpolation = cv.INTER_AREA)
+
+                _1, bestValue, _, bestLocation = cv.minMaxLoc(cv.matchTemplate(screenImg, scaledTempImg, cv.TM_CCORR_NORMED, None, scaledMaskImg))
+                
+                print(_1, bestValue, sep = ' ')
+                
+                return tuple[bestValue, bestLocation]
+        
+        def _TakeScreenshot(self):
+                
+                return cv.cvtColor(np.array(IG.grab().convert("RGB"))[:, :, ::-1], cv.COLOR_BGR2GRAY)
+                
 
 
 sh = ScreenHandler()
 
-sh.FindBestPosition()
+aio.run(sh.FindChessboardPosition())
 
 # https://pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/
 # https://stackoverflow.com/questions/35642497/python-opencv-cv2-matchtemplate-with-transparency
 # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_core/py_basic_ops/py_basic_ops.html
 # https://post.bytes.com/forum/topic/python/22175-multiply-a-tuple-by-a-constant?t=28276
+# https://roboflow.com/use-opencv/convert-pil-image-to-cv2-image#:~:text=You%20can%20convert%20a%20PIL,BGR%20(the%20cv2%20format).
 # some parts of these sources are used together to come up with the algorithm above
